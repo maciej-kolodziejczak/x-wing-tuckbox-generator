a32274f55965908f677b28af9617c639fe19df33
Maciej Kołodziejczak <mkolodziejczak@worldremit.com>
2020-09-01 19:56:04 +0200

    add basic renderer idea

--- src/pages/UpgradeCards/UpgradeCards.tsx
+++ src/pages/UpgradeCards/UpgradeCards.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from "react";
 import { Container } from "../../components/Container/Container";
+import { Renderer } from "../../components/Renderer/Renderer";
 
 export const UpgradeCards: FC<{}> = () => {
   return (
     <div>
-      <Container>Upgrade Cards</Container>
+      <Container>
+        Upgrade Cards <Renderer />
+      </Container>
     </div>
   );
 };
